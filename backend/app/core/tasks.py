0eed980587d33316d67b7c4b5a54a798604c0c66
n4naynay <nenedurugo@yahoo.co.uk>
2024-05-11 15:38:48 +0100

    Code updates

---
+++ backend/app/core/tasks.py
@@ -0,0 +1,19 @@
+#this task in the core speaks to the task in the db folder
+from typing import Callable
+from fastapi import FastAPI
+
+from app.db.tasks import connect_to_db, close_db_connection
+
+
+def create_start_app_handler(app: FastAPI) -> Callable:
+    async def start_app() -> None:
+        await connect_to_db(app)
+
+    return start_app
+
+
+def create_stop_app_handler(app: FastAPI) -> Callable:
+    async def stop_app() -> None:
+        await close_db_connection(app)
+
+    return stop_app
